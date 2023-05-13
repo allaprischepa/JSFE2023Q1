f4d8a075973e7aeedea931294ee5d4b27c6b1ded
Alla Prishchepa <tyurina.alla@gmail.com>
2023-05-14 00:19:40 +0400

    feat: implement switchers, add icons

--- minesweeper/src/scripts/minesweeper.js
+++ minesweeper/src/scripts/minesweeper.js
@@ -1,19 +1,41 @@
 export default class Minesweeper {
+  /**
+   * Initialize all variables and blocks.
+   */
   init() {
     this.selectors = {
       main: 'minesweeper',
       mode: 'minesweeper__mode',
       minesCount: 'minesweeper__mines-count',
       field: 'minesweeper__field',
+      theme: 'minesweeper__theme',
     };
-    this.currentMode = 'junior';
-    this.currentMinesCount = 10;
+
+    this.mode = localStorage.getItem('mode') || 'easy';
+    this.minesCount = localStorage.getItem('minesCount') || 10;
+    this.theme = localStorage.getItem('theme') || 'light';
+    this.sound = localStorage.getItem('sound') || 'on';
+
     this.body = document.querySelector('body');
+    this.body.className = 'page';
+    document.documentElement.className = `theme-${this.theme}`;
+
     this.mainBlock = this.addMainBlock();
+    this.themeSwitcherBlock = this.addThemeSwitcherBlock();
+    this.soundSwitcherBlock = this.addSoundSwitcherBlock();
     [this.modeBlock, this.countBlock] = this.addSettingsBlock();
     this.gameFieldBlock = this.addGameFieldBlock();
+
+    this.listenThemeChange();
+    this.listenSoundChange();
+    this.listenModeChange();
+    this.listenMinesCountChange();
   }
 
+  /**
+   * Add main Minesweeper block.
+   * @returns
+   */
   addMainBlock() {
     const mainBlock = document.createElement('div');
     mainBlock.classList = this.selectors.main;
@@ -22,35 +44,82 @@ export default class Minesweeper {
     return mainBlock;
   }
 
+  /**
+   * Add Theme switcher.
+   * @returns
+   */
+  addThemeSwitcherBlock() {
+    const themeSwitcherBlock = document.createElement('div');
+    themeSwitcherBlock.classList = this.selectors.theme;
+
+    const input = document.createElement('input');
+    const inputID = 'theme-switcher';
+    input.setAttribute('type', 'checkbox');
+    input.setAttribute('id', inputID);
+    input.classList = 'theme-switcher__input';
+    if (this.theme === 'light') input.checked = true;
+
+    const span = document.createElement('span');
+    span.classList = 'theme-switcher__toggle';
+
+    const label = document.createElement('label');
+    label.setAttribute('for', inputID);
+    label.appendChild(input);
+    label.appendChild(span);
+
+    themeSwitcherBlock.appendChild(label);
+    this.mainBlock.appendChild(themeSwitcherBlock);
+
+    return themeSwitcherBlock;
+  }
+
+  /**
+   * Add Sound switcher.
+   * @returns
+   */
+  addSoundSwitcherBlock() {
+    const soundSwitcherBlock = document.createElement('div');
+    soundSwitcherBlock.classList = this.selectors.theme;
+
+    const input = document.createElement('input');
+    const inputID = 'sound-switcher';
+    input.setAttribute('type', 'checkbox');
+    input.setAttribute('id', inputID);
+    input.classList = 'sound-switcher__input';
+    if (this.sound === 'on') input.checked = true;
+
+    const span = document.createElement('span');
+    span.classList = 'sound-switcher__toggle';
+
+    const label = document.createElement('label');
+    label.setAttribute('for', inputID);
+    label.appendChild(input);
+    label.appendChild(span);
+
+    soundSwitcherBlock.appendChild(label);
+    this.mainBlock.appendChild(soundSwitcherBlock);
+
+    return soundSwitcherBlock;
+  }
+
+  /**
+   * Add Minesweeper Settings block.
+   * @returns
+   */
   addSettingsBlock() {
+    // Create Mode block.
+    const modes = ['easy', 'medium', 'hard'];
     const modeBlock = document.createElement('div');
-    const modes = ['junior', 'middle', 'senior'];
-    const countBlock = document.createElement('div');
-    const countInput = document.createElement('input');
-    const countLabel = document.createElement('label');
-    const rangeID = 'count';
-    const rangeAttrs = {
-      type: 'range',
-      min: 10,
-      max: 99,
-      step: 1,
-      id: rangeID,
-      name: rangeID,
-      value: this.currentMinesCount,
-    };
-
     modeBlock.classList = this.selectors.mode;
-    countBlock.classList = this.selectors.minesCount;
-    countLabel.innerText = 'Mines count';
-    countLabel.setAttribute('for', rangeID);
-    Object.entries(rangeAttrs).forEach(([key, value]) => {
-      countInput.setAttribute(key, value);
-    });
 
-    this.mainBlock.appendChild(modeBlock);
-    countBlock.appendChild(countLabel);
-    countBlock.appendChild(countInput);
-    this.mainBlock.appendChild(countBlock);
+    const modeBlockName = document.createElement('div');
+    modeBlockName.classList = 'setting-name';
+    modeBlockName.innerText = 'Mode';
+    modeBlock.appendChild(modeBlockName);
+
+    const modesWrapper = document.createElement('div');
+    modesWrapper.classList = 'setting-block';
+    modeBlock.appendChild(modesWrapper);
 
     modes.forEach((mode) => {
       const label = document.createElement('label');
@@ -66,19 +135,62 @@ export default class Minesweeper {
       Object.entries(inputAttrs).forEach(([key, value]) => {
         input.setAttribute(key, value);
       });
-      if (mode === this.currentMode) {
+      if (mode === this.mode) {
         input.checked = true;
       }
       label.setAttribute('for', inputID);
-      label.innerText = mode;
+      label.innerText = Minesweeper.capitalize(mode);
 
       label.appendChild(input);
-      modeBlock.appendChild(label);
+      modesWrapper.appendChild(label);
     });
+
+    // Create Mines count block.
+    const countBlock = document.createElement('div');
+    countBlock.classList = this.selectors.minesCount;
+
+    const countBlockName = document.createElement('div');
+    countBlockName.classList = 'setting-name';
+    countBlockName.innerText = 'Mines';
+    countBlock.appendChild(countBlockName);
+
+    const countWrapper = document.createElement('div');
+    countWrapper.classList = 'setting-block';
+    countBlock.appendChild(countWrapper);
+
+    const countInput = document.createElement('input');
+    const countValue = document.createElement('span');
+    countValue.classList = 'mines-count__value';
+    countValue.innerText = this.minesCount;
+
+    const rangeID = 'count';
+    const rangeAttrs = {
+      type: 'range',
+      min: 10,
+      max: 99,
+      step: 1,
+      id: rangeID,
+      name: rangeID,
+      value: this.minesCount,
+    };
+
+    Object.entries(rangeAttrs).forEach(([key, value]) => {
+      countInput.setAttribute(key, value);
+    });
+
+    countWrapper.appendChild(countInput);
+    countWrapper.appendChild(countValue);
+
+    this.mainBlock.appendChild(modeBlock);
+    this.mainBlock.appendChild(countBlock);
 
     return [modeBlock, countBlock];
   }
 
+  /**
+   * Add Minesweeper field block.
+   * @returns
+   */
   addGameFieldBlock() {
     const gameFieldBlock = document.createElement('div');
     const rowsCount = 10;
@@ -93,8 +205,92 @@ export default class Minesweeper {
       }
     }
 
-    this.body.appendChild(gameFieldBlock);
+    this.mainBlock.appendChild(gameFieldBlock);
 
     return gameFieldBlock;
+  }
+
+  /**
+   * Listen changing of theme.
+   */
+  listenThemeChange() {
+    const input = this.themeSwitcherBlock.querySelector('input');
+
+    input.addEventListener('change', () => {
+      const newTheme = this.theme === 'light' ? 'dark' : 'light';
+
+      this.updateProperty('theme', newTheme);
+      document.documentElement.className = `theme-${newTheme}`;
+    });
+  }
+
+  /**
+   * Listen changing of sound settings.
+   */
+  listenSoundChange() {
+    const input = this.soundSwitcherBlock.querySelector('input');
+
+    input.addEventListener('change', () => {
+      const newSetting = this.sound === 'on' ? 'off' : 'on';
+
+      this.updateProperty('sound', newSetting);
+    });
+  }
+
+  /**
+   * Listen changing of game mode.
+   */
+  listenModeChange() {
+    const radios = this.modeBlock.querySelectorAll('input[name="mode"]');
+    radios.forEach((radio) => {
+      radio.addEventListener('change', (event) => {
+        const { target } = event;
+        const currentValue = target.getAttribute('value');
+
+        this.updateProperty('mode', currentValue);
+      });
+    });
+  }
+
+  /**
+   * Listen changing of mines count.
+   */
+  listenMinesCountChange() {
+    const input = this.countBlock.querySelector('input[name="count"]');
+    const span = this.countBlock.querySelector('.mines-count__value');
+
+    input.addEventListener('input', (event) => {
+      span.innerText = event.target.value;
+    });
+
+    input.addEventListener('change', (event) => {
+      const { target } = event;
+      const currentValue = target.value;
+
+      target.setAttribute('value', currentValue);
+      this.updateProperty('minesCount', currentValue);
+    });
+  }
+
+  /**
+   * Update Minesweeper object property and localStorage item.
+   * @param {*} property
+   * @param {*} value
+   */
+  updateProperty(property, value) {
+    localStorage.setItem(property, value);
+    this[property] = value;
+  }
+
+  /**
+   * Transform first letter of the word to upper case.
+   * @param {*} word
+   * @returns
+   */
+  static capitalize(word) {
+    const firstLetter = word.charAt(0).toUpperCase();
+    const otherLetters = word.slice(1);
+
+    return `${firstLetter}${otherLetters}`;
   }
 }
