af9bb59f08d69137c973e8fe0c2f50990de54caf
Alla Prishchepa <tyurina.alla@gmail.com>
2023-06-06 23:11:56 +0400

    feat: add my own API key

--- news-api/src/components/controller/appLoader.js
+++ news-api/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'e738afbb060f488bb66621a6b75efacc', // получите свой ключ https://newsapi.org/
         });
     }
 }
