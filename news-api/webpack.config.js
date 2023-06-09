bf5806cadc3d14a1ace7af7433d1c989e54ac1ba
Alla Prishchepa <tyurina.alla@gmail.com>
2023-06-09 16:47:12 +0400

    chore: change webpack config

--- news-api/webpack.config.js
+++ news-api/webpack.config.js
@@ -25,12 +25,13 @@ const baseConfig = {
     },
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, '../dist'),
+        path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, './src/index.html'),
             filename: 'index.html',
+            inject: 'body',
         }),
         new CleanWebpackPlugin(),
         new EslingPlugin({ extensions: 'ts' }),
